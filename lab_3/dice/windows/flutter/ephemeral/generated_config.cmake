2b8230e37a4d6f588664499383b0a93e3b50640f
Ductq0705 <truongquocduc0705@gmail.com>
2024-09-10 21:00:47 +0700

    lab_3

---
+++ lab_3/dice/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\lab_flutter\\Dice App\\dice" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter"
+  "PROJECT_DIR=D:\\lab_flutter\\Dice App\\dice"
+  "FLUTTER_ROOT=D:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\lab_flutter\\Dice App\\dice\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\lab_flutter\\Dice App\\dice"
+  "FLUTTER_TARGET=D:\\lab_flutter\\Dice App\\dice\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\lab_flutter\\Dice App\\dice\\.dart_tool\\package_config.json"
+)
